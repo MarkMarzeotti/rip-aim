d9ede33ae95e3e7c89236ad113cdf4df1b9edcee
Mark <mmarzeotti@redventures.com>
2017-12-14 12:59:41 -0500

    final edits and finishing touches

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
-var pageLink = 'https://ripaim.markmarzeotti.com/';
-// var pageLink = 'http://localhost:8888/personal/rip-aim/';
+// var pageLink = 'https://ripaim.markmarzeotti.com/';
+//var pageLink = 'http://localhost:8888/personal/rip-aim/';
+var pageLink = 'https://aimliveson.com/';
+
+// ReCAPTCHA Secret Key
+// 6LdOzDwUAAAAAHTRRn4GmQtao7cB6W7LlJWovvsR
 
 var a2a_config = a2a_config || {};
-a2a_config.linkurl = pageLink + "assets/img/generator-bg.png";
+a2a_config.linkurl = pageLink + "assets/img/aol-instant-messenger.png";
 
 a2a_config.templates = a2a_config.templates || {};
 
@@ -19,58 +23,66 @@ window.onload = function(){
 
   var buryScreenname = function(e) {
     e.preventDefault();
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    var imageObj = new Image();
-    imageObj.onload = function(){
-      context.drawImage(imageObj, 0, 0);
 
-      var x = canvas.width / 2;
-      var y = canvas.height / 3 + 134;
-      var screenname = screennameInput.value;
+    // log ip address incase I want to lock it down for too many requests
+    // $.getJSON('//api.ipify.org?format=jsonp&callback=?', function(data) {
+    //   console.log(data.ip);
+    // });
 
-      context.font = '69px apple';
-      context.textAlign = 'center';
-      context.fillStyle = '#080881';
-      context.fillText(screenname, x, y);
+    var screenname = screennameInput.value;
+    if (screenname.length >= 3 && screenname.length <= 16) {
+      var canvas = document.getElementById("canvas");
+      var context = canvas.getContext("2d");
+      var imageObj = new Image();
+      imageObj.onload = function(){
+        context.drawImage(imageObj, 0, 0);
 
-      var canvasData = canvas.toDataURL("image/png");
+        var x = canvas.width / 2;
+        var y = canvas.height / 3 + 134;
 
-      var base64Image = canvasData.split(';base64,').pop();
+        context.font = '69px apple';
+        context.textAlign = 'center';
+        context.fillStyle = '#080881';
+        context.fillText(screenname, x, y);
 
-      // Fire off the request to save.php
-      request = $.ajax({
-          url: 'save.php',
-          method: "POST",
-          data: { image: base64Image, screenname: screenname },
-          dataType: "html"
-      });
+        var canvasData = canvas.toDataURL("image/png");
 
-      request.done(function (response, textStatus, jqXHR){
-          // Log a message to the console
-          console.log("image created");
+        var base64Image = canvasData.split(';base64,').pop();
 
-          // next we need to update all the share meta
-          $('.a2a_kit').attr('data-a2a-url', pageLink + '?screenname=' + screenname);
-          a2a_config.linkurl = pageLink + "?screenname=" + screenname;
+        // Fire off the request to save.php
+        request = $.ajax({
+            url: 'save.php',
+            method: "POST",
+            data: { image: base64Image, screenname: screenname },
+            dataType: "html"
+        });
 
-          genBucket.html('<img src="' + pageLink + 'generated/' + screenname + '.png">');
-          $('.aol h1').remove();
-          $('.aol .create-image').remove();
-          $('.aol .share').show();
-          $('.aol .a2a_kit .download').attr('href', 'download.php?file=generated/' + screenname + '.png');
-      });
+        request.done(function (response, textStatus, jqXHR){
+            // Log a message to the console
+            console.log("image created");
 
-      request.fail(function (jqXHR, textStatus, errorThrown){
-          // Log the error to the console
-          console.error(
-              "The following error occurred: "+
-              textStatus, errorThrown
-          );
-      });
+            // next we need to update all the share meta
+            $('.a2a_kit').attr('data-a2a-url', pageLink + '?screenname=' + screenname);
+            a2a_config.linkurl = pageLink + "?screenname=" + screenname;
 
-    };
-    imageObj.src = "./assets/img/generator-bg.png";
+            genBucket.html('<img src="' + pageLink + 'generated/' + screenname + '.png">').show();
+            $('.aol h1').remove();
+            $('.aol .create-image').remove();
+            $('.aol .share').show();
+            $('.aol .a2a_kit .download').attr('href', 'download.php?file=generated/' + screenname + '.png');
+        });
+
+        request.fail(function (jqXHR, textStatus, errorThrown){
+            // Log the error to the console
+            console.error(
+                "The following error occurred: "+
+                textStatus, errorThrown
+            );
+        });
+
+      };
+      imageObj.src = "./assets/img/generator-bg.png";
+    }
   };
 
   var bury = document.getElementById("bury");
--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
-    $pageLink = 'https://ripaim.markmarzeotti.com/';
+    if($_SERVER["HTTPS"] != "on") {
+        header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        exit();
+    }
+
+    // $pageLink = 'https://ripaim.markmarzeotti.com/';
     // $pageLink = 'http://localhost:8888/personal/rip-aim/';
+    $pageLink = 'https://aimliveson.com/';
     $screenname = htmlspecialchars($_GET["screenname"]); // between 3 and 16 chars
     if ($screenname) {
-        $title = $screenname . ' Shared Their Image';
+        $title = 'AIM Lives On';
         $image = $pageLink . 'generated/' . $screenname . '.png';
         $link = $pageLink . '?screenname=' . $screenname;
-        $blurb = 'Share your image with this fun little tool!';
+        $blurb = 'AIM may be gone, but your screenname can live on forever.';
     } else {
         $title = 'AIM Lives On';
-        $image = $pageLink . 'assets/img/generator-bg.png';
+        $image = $pageLink . 'assets/img/aol-instant-messenger.png';
         $link = $pageLink;
-        $blurb = 'Share your image with this fun little tool!';
+        $blurb = 'Immortalize your screenname';
     }
 ?>
 
@@ -20,7 +26,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title><?php echo $title; ?></title>
+        <title><?php echo $title . ' - ' . $blurb; ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" initial-scale="1">
 
         <meta property="og:image" content="<?php echo $image; ?>" />
@@ -45,12 +51,18 @@
 
         <meta property="fb:app_id" content="966242223397117" />
 
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
         <meta name="Description" content="<?php echo $blurb; ?>" />
 
         <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
         <link rel="canonical" href="<?php echo $pageLink; ?>"/>
 
         <link rel="stylesheet" href="assets/css/main.min.css">
+
+        <?php /*<script src='https://www.google.com/recaptcha/api.js'></script>*/ ?>
     </head>
     <body>
         <!--[if lte IE 9]>
@@ -66,6 +78,7 @@
             <div class="corner-top-right"></div>
             <div class="corner-bottom-left"></div>
             <div class="corner-bottom-right"></div>
+            <div class="icon-tomb"></div>
             <div class="page-title">
                 <?php echo $title; ?>
             </div>
@@ -76,40 +89,51 @@
                 <div class="vertical-middle">
 
                     <div class="row">
-                        <div class="aol column text-center">
-                            <h1>AIM MAY BE GONE, BUT YOUR SCREENNAME CAN LIVE ON FOREVER.</h1>
-                            <canvas id="canvas" width="1200" height="630"></canvas>
-                            <?php if ($screenname) { ?>
-                            <div class="created-image">
-                                <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>">
-                            </div>
-                            <a href="<?php echo $pageLink; ?>">Create Yours!</a>
-                            <?php } else { ?>
-                            <div class="gen-bucket"></div>
-                            <div class="create-image">
-                                <img src="./assets/img/aol-instant-messenger.png" alt="RIP AIM" />
-                                <hr />
-                                <h2>Immortalize your ScreenName here:</h2>
-                                <form class="image-generator" id="bury" action="<?php echo $pageLink; ?>">
-                                    <input type="text" name="screenname" id="screenname" value="" maxlength="16">
-                                    <input type="submit" value="Sign On Forever">
-                                </form>
-                            </div>
-                            <?php } ?>
-                            <div class="share">
+                        <div class="aol column">
 
-                                <!-- AddToAny BEGIN -->
+                            <canvas id="canvas" width="1200" height="630"></canvas>
+
+                            <div class="gen-bucket"></div>
+
+                            <div class="create-image">
+                                <div class="gif-left">
+                                    <img src="assets/img/rip.gif" alt="RIP Tombstone" />
+                                </div>
+                                <div class="form">
+                                    <div class="text-left">
+                                        <h1>AIM MAY BE GONE, BUT YOUR SCREENNAME CAN LIVE ON FOREVER.</h1>
+                                        <p>On December 15th, 2017 AIM signs off for good. But we don't want your screenname to sign off with it. For many of us, our AIM screenname was our first online identity. So we created a place where those (not at all embarrassing) screennames can live on forever.</p>
+                                    </div>
+                                    <div class="text-center">
+                                        <hr />
+                                        <h2>Immortalize your ScreenName here:</h2>
+                                        <form class="image-generator" id="bury" action="<?php echo $pageLink; ?>">
+                                            <input type="text" name="screenname" id="screenname" value="" maxlength="16">
+                                            <?php /*<div class="g-recaptcha" data-sitekey="6LdOzDwUAAAAAAXifkXvDAW0nVHDNCJscTuFzCBb"></div>*/ ?>
+                                            <input type="submit" value="Sign On Forever">
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="gif-right">
+                                    <img src="assets/img/aim.gif" alt="AIM Tombstone" />
+                                </div>
+                            </div>
+
+                            <div class="share">
                                 <div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?php echo $link; ?>">
                                     <a class="a2a_button_facebook"></a>
                                     <a class="a2a_button_twitter"></a>
+                                    <a class="a2a_button_linkedin"></a>
                                     <a class="download"></a>
                                 </div>
                                 <script async src="https://static.addtoany.com/menu/page.js"></script>
-                                <!-- AddToAny END -->
                             </div>
                         </div>
                     </div>
+                </div>
 
+                <div class="digo">
+                    <p><small>Brought to you by DiMassimo Goldstein - A New York inspiring action agency</small></p>
                 </div>
             </div>
         </section>
@@ -118,5 +142,15 @@
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
         <script src="assets/js/main.min.js"></script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-24799650-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-24799650-1');
+        </script>
     </body>
 </html>
